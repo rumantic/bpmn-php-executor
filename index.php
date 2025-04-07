fbccd7ba546e40a6dad74648e95ce0e178d672bc
Kondin Dmitriy <dkondin@gmail.com>
2025-04-07 12:49:59 +0700

    пошаговый проход по простому алгоритму

--- index.php
+++ index.php
@@ -3,50 +3,71 @@
 use App\CustomParticipant;
 use PHPMentors\Workflower\Definition\Bpmn2Reader;
 
+session_start();
 require_once "vendor/autoload.php";
 
+// Загрузка BPMN-файла
 $bpmn2Reader = new Bpmn2Reader();
-//$workflow = $bpmn2Reader->read('vendor/phpmentors/workflower/tests/Resources/config/workflower/LoanRequestProcess.bpmn');
-$workflow = $bpmn2Reader->read('c.bpmn');
+// $workflow = $bpmn2Reader->read('c.bpmn');
+$workflow = $bpmn2Reader->read('long-simple.bpmn');
+
+// Инициализация участника
 $participant = new CustomParticipant(['ROLE_BRANCH', 'ROLE_CREDIT_FACTORY', 'ROLE_BACK_OFFICE', '__ROLE__']);
 
+// Проверка текущего шага в сессии
+if (!isset($_SESSION['currentStep'])) {
+    // Получение идентификатора StartEvent
+    $startEvent = $workflow->getFlowObject('StartEvent_1'); // Замените 'Start' на реальный ID из BPMN-файла
+    if ($startEvent === null) {
+        die("Ошибка: StartEvent с идентификатором 'Start' не найден в BPMN-файле.");
+    }
 
+    $workflow->start($startEvent);
+    $_SESSION['workflow'] = serialize($workflow);
+    $_SESSION['currentStep'] = $workflow->getCurrentFlowObject()->getId();
+} else {
+    $workflow = unserialize($_SESSION['workflow']);
+    $workflow->setProcessData(['rejected' => $_SESSION['rejected'] ?? false]);
+}
 
-$workflow->setProcessData(array('rejected' => false));
-$workflow->start($workflow->getFlowObject('Start'));
+// Получение текущего шага
+$currentFlowObject = $workflow->getFlowObject($_SESSION['currentStep']);
 
-$currentFlowObject = $workflow->getCurrentFlowObject();
-echo $currentFlowObject->getName().'<br>';
+// Если это условный переход, обработать выбор пользователя
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['choice'])) {
+    $_SESSION['rejected'] = ($_POST['choice'] === 'yes');
+    $workflow->setProcessData(['rejected' => $_SESSION['rejected']]);
+}
 
-$workflow->allocateWorkItem($workflow->getCurrentFlowObject(), $participant);
-$workflow->startWorkItem($workflow->getCurrentFlowObject(), $participant);
-$workflow->completeWorkItem($workflow->getCurrentFlowObject(), $participant);
+// Выполнение текущего шага
+if (!$workflow->isEnded()) {
+    $workflow->allocateWorkItem($currentFlowObject, $participant);
+    $workflow->startWorkItem($currentFlowObject, $participant);
+    $workflow->completeWorkItem($currentFlowObject, $participant);
 
-$currentFlowObject = $workflow->getCurrentFlowObject();
-echo $currentFlowObject->getName().'<br>';
+    // Обновление текущего шага
+    $currentFlowObject = $workflow->getCurrentFlowObject();
+    $_SESSION['currentStep'] = $currentFlowObject->getId();
+    $_SESSION['workflow'] = serialize($workflow);
+}
 
+// Генерация HTML-страницы
+if ($workflow->isEnded()) {
+    echo "<h1>Процесс завершен!</h1>";
+    session_destroy();
+} else {
+    echo "<h1>Текущий шаг: " . htmlspecialchars($currentFlowObject->getName()) . "</h1>";
 
-$workflow->allocateWorkItem($workflow->getCurrentFlowObject(), $participant);
-$workflow->startWorkItem($workflow->getCurrentFlowObject(), $participant);
-$workflow->setProcessData(array('rejected' => false));
-
-$workflow->completeWorkItem($workflow->getCurrentFlowObject(), $participant);
-
-$currentFlowObject = $workflow->getCurrentFlowObject();
-echo $currentFlowObject->getName().'<br>';
-
-$workflow->allocateWorkItem($workflow->getCurrentFlowObject(), $participant);
-$workflow->startWorkItem($workflow->getCurrentFlowObject(), $participant);
-
-$workflow->completeWorkItem($workflow->getCurrentFlowObject(), $participant);
-
-$currentFlowObject = $workflow->getCurrentFlowObject();
-echo $currentFlowObject->getName().'<br>';
-
-$workflow->allocateWorkItem($workflow->getCurrentFlowObject(), $participant);
-$workflow->startWorkItem($workflow->getCurrentFlowObject(), $participant);
-
-$workflow->completeWorkItem($workflow->getCurrentFlowObject(), $participant);
-
-$currentFlowObject = $workflow->getCurrentFlowObject();
-echo $currentFlowObject->getName().'<br>';
+    // Если это условный переход, показать выбор
+    if ($currentFlowObject instanceof \PHPMentors\Workflower\Workflow\Gateway\ExclusiveGateway) {
+        echo "<form method='POST'>";
+        echo "<p>Выберите действие:</p>";
+        echo "<button type='submit' name='choice' value='yes'>Да</button>";
+        echo "<button type='submit' name='choice' value='no'>Нет</button>";
+        echo "</form>";
+    } else {
+        echo "<form method='POST'>";
+        echo "<button type='submit'>Далее</button>";
+        echo "</form>";
+    }
+}
